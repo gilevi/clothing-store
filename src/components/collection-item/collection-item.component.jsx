94124f2ed8b8b584a904d2cdd9fc6660ad1bd83b
gil <gil.levi@sap.com>
2022-03-07 18:27:24 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresnponding component with ux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,7 +7,7 @@ import { addItem } from "../../redux/cart/cart.actions";
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem}) => {
-    const {imageUrl, name, price, id} = item;
+    const {imageUrl, name, price} = item;
     return (
     <div className="collection-item">
         <div className="image" 
